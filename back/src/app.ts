16fe8a8114cbe8c9874fe53efaf84abc711b0ca0
Oscar Martinez <oscarmartinez1998lol@gmail.com>
2022-07-14 15:26:23 -0500

    Database connection set it up, test folder initial implementation

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,9 +1,12 @@
 import createServer from 'server';
+import dbConnection from 'dbConnection';
+import logger from 'logger';
 
 const startServer = async () => {
   const app = createServer();
+  await dbConnection();
   app.listen(app.get('PORT'), () => {
-    console.log(`Server running on port ${app.get('PORT')}`);
+    logger.info(`Server running on port ${app.get('PORT')}`);
   });
 };
 
